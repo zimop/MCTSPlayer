91466e3eb8576c067977fffade3e989cfb8c8827
zimop <zimopeng12345@gmail.com>
2023-05-18 17:53:32 +1000

    testing submission

--- agents/t_053/myTeam.py
+++ agents/t_053/myTeam.py
@@ -8,65 +8,83 @@ from Azul.azul_model import AzulGameRule as GameRule
 from Azul.azul_model import AzulState as GameState
 from itertools import count
 
-THINKTIME   = 0.9
+THINKTIME   = 0.5
 NUM_PLAYERS = 2
+Q_REWARDS = dict()
 
 class myAgent(Agent):
     def __init__(self,_id):
         super().__init__(_id)
         self.game_rule = GameRule(NUM_PLAYERS)
-        self.game_state = GameState(NUM_PLAYERS)
-    
-    def isGoal(self, state, action):
-        numRowsCompleted = 0
-        numRowsCompletedSucc = 0
-        for i in range(state.agents[self.id].GRID_SIZE):
-            if (state.agents[self.id].lines_number[i] == i+1):
-                numRowsCompleted += 1
-                
-        state = self.game_rule.generateSuccessor(state, action, self.id)
-        
-        for j in range(state.agents[self.id].GRID_SIZE):
-            if (state.agents[self.id].lines_number[j] == j+1):
-                numRowsCompletedSucc += 1
-                
-        return numRowsCompletedSucc > numRowsCompleted
     
     def GetActions(self, state):
         return self.game_rule.getLegalActions(state, self.id)
     
-    def heuristic(self, state):
-        arrayOnFloor = state.agents[self.id].floor
-        numOnFloor = 0
-        for val in arrayOnFloor:
-            if val == 1:
-                numOnFloor += 1
-        return -numOnFloor
-    
     def SelectAction(self,actions,root_state):
-        unique = count()
         start_time = time.time()
-        myPQ = []
-        node = (deepcopy(root_state), [])
-        hValue = self.heuristic(root_state)
-        heapq.heappush(myPQ, (hValue, next(unique),  node))
-        ##myPQ = deque([node])
-        closedStates = []
-        while len(myPQ) and time.time()-start_time < THINKTIME:
-            whole = heapq.heappop(myPQ)
-            _,_, currNode = whole
-            state, path = currNode
-            newActions = self.GetActions(state)
-            if (state not in closedStates):
-                closedStates = closedStates + [state]
-                for a in newActions:
-                    next_path = path + [a]
+        gamma = 0.9
+        start = deepcopy(root_state)
+        count = 0
+        while time.time()-start_time < THINKTIME:
+            count += 1
+            openList = [start]
+            closedList = []
+            while len(openList) and time.time()-start_time < THINKTIME:
+                state = openList.pop(0)
+                closedList.append(state)
+                new_actions = self.GetActions(state)
+                
+                firstAction = new_actions[0]
+                firstState = deepcopy(state)
+                
+                self.game_rule.generateSuccessor(firstState, firstAction, self.id)
+                
+                first_futureScore = firstState.agents[self.id].ScoreRound()[0]
+                first_futureBonus = firstState.agents[self.id].EndOfGameScore()
+                first_future_reward = first_futureScore + first_futureBonus
+                
+                max_state = firstState
+                max_action = firstAction
+                max_future_reward = first_future_reward 
+                
+                
+                #future_reward = 0
+                
+                
+                for a in new_actions[1:]:
+                    
+                    
+                    future_reward = 0
                     next_state = deepcopy(state)
-                    if(self.isGoal(next_state, a)):
-                        return next_path[0]
+                    
+                    self.game_rule.generateSuccessor(next_state, a, self.id)
+                    
+                    
+                    if (next_state not in closedList):
+                        openList.append(next_state)
+                        
+                    if (next_state in Q_REWARDS.keys()):
+                        future_reward, _, _ = Q_REWARDS[next_state]
+                        
                     else:
-                        hValue = self.heuristic(root_state)
-                        newNode = (next_state, next_path)
-                        heapq.heappush(myPQ, (hValue, next(unique), newNode))
-                        ##myPQ.append(newNode)
-        return random.choice(actions)
+                        futureScore = next_state.agents[self.id].ScoreRound()[0]
+                        futureBonus = next_state.agents[self.id].EndOfGameScore()
+                        future_reward = futureScore + futureBonus
+                        
+                    if (future_reward > max_future_reward):
+                        max_future_reward = future_reward
+                        max_state = next_state
+                        max_action = a
+                
+                currState = deepcopy(state)        
+                scoreChange = currState.agents[self.id].ScoreRound()[0]
+                bonus = currState.agents[self.id].EndOfGameScore()
+                reward = scoreChange + bonus
+                
+                
+                #Opponent
+                
+                
+                Q_REWARDS[state] = ((reward + gamma*max_future_reward), max_state, max_action)
+              
+        return Q_REWARDS[start][2]
